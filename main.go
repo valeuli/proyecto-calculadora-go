6a1aad52804b15254ae0ea777025e8489b7b2a8e
Juliana <valeuli12@gmail.com>
2025-11-01 17:10:27 -0500

    Añadido manejo de operaciones vía API REST y cálculo dinámico

--- main.go
+++ main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type Operacion struct {
+	Operando1 float64 `json:"operando1"`
+	Operando2 float64 `json:"operando2"`
+	Operador  string  `json:"operador"`
+	Resultado float64 `json:"resultado,omitempty"`
+	Error     string  `json:"error,omitempty"`
+}
+
+func (op *Operacion) Calcular() {
+	switch op.Operador {
+	case "+":
+		op.Resultado = op.Operando1 + op.Operando2
+	case "-":
+		op.Resultado = op.Operando1 - op.Operando2
+	case "*":
+		op.Resultado = op.Operando1 * op.Operando2
+	case "/":
+		if op.Operando2 == 0 {
+			op.Error = "Error: División por cero"
+		} else {
+			op.Resultado = op.Operando1 / op.Operando2
+		}
+	default:
+		op.Error = "Operador no válido. Usa +, -, * o /"
+	}
+}
+
+func manejarOperacion(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var op Operacion
+	err := json.NewDecoder(r.Body).Decode(&op)
+	if err != nil {
+		http.Error(w, "Error al decodificar el cuerpo", http.StatusBadRequest)
+		return
+	}
+
+	op.Calcular()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(op)
+}
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	http.HandleFunc("/operacion", manejarOperacion)
+	fmt.Println("Servidor en ejecución en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
